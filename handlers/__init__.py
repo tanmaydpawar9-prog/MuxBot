4751dad19e5b10276f3e38d12131200233502d3c
Tanmay Pawar <tanmaydpawar9@gmail.com>
2026-04-11 16:53:44 +0530

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .mux import register_mux_handlers
-from .subtitle import register_subtitle_handlers
-from .cache import register_cache_handlers
-from .speed import register_speed_handlers
-from .control import register_control_handlers
